5ed53bdef99db9183c9899ccdca51c43c350c74c
Alex Yale-Loehr <ayaleloehr@gmail.com>
2016-03-01 23:59:07 -0500

    fixing lint errors

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,55 +10,72 @@ class SearchController < ApplicationController
     respond_to do |format|
       format.html
       format.json do
-        if params[:service_level_2].nil? and params[:service_level_1].nil?
-          @facilities = Facility.all
-        else
-          if !params[:service_level_2].nil?
-            facility_ids_that_match = Service.where(service_level_2: params[:service_level_2]).uniq.pluck(:facility_id)
-          else
-            facility_ids_that_match = Service.where(service_level_1:
-                          params[:service_level_1]).uniq.pluck(:facility_id)
-          end
-          @facilities = Facility.where(facility_id: facility_ids_that_match)
-        end
-        render json: @facilities
+        render json: @facilities = if params[:service_level_2].nil? && params[:service_level_1].nil?
+                                     Facility.all
+                                   else
+                                     get_facilities_by_service(params)
+                                   end
       end
     end
+  end
+
+  def get_facilities_by_service(params)
+    if !params[:service_level_2].nil?
+      facility_ids_that_match = Service.where(service_level_2:
+                                                  params[:service_level_2]).uniq.pluck(:facility_id)
+    else
+      facility_ids_that_match = Service.where(service_level_1:
+                                                  params[:service_level_1]).uniq.pluck(:facility_id)
+    end
+    Facility.where(facility_id: facility_ids_that_match)
   end
 
   def single_facility
     @facility = Facility.where(facility_id: params[:facility_id]).first
     services = Service.where(facility_id: params[:facility_id])
-    @servicesRolledUp = []
-    services.each do | service |
-      unless @servicesRolledUp.include?(service.service_level_1)
-        @servicesRolledUp << service.service_level_1
-      end
-    end
-    @servicesHash = {}
-    @servicesRolledUp.each do | serviceLevel1 |
-      tempArray = []
-      services.each do | service |
-        if serviceLevel1 == service.service_level_1
-          if service.service_level_2 != nil
-            tempArray << service.service_level_2
-          end
-        end
-        @servicesHash[serviceLevel1] = tempArray
-      end
-    end
-    i = 2
+    @services_hash = generate_hash_of_service_levels(services)
   end
-  
+
   def service_types_by_administration
-    #add in test for if health or benefits or NCA and redirect accordingly
+    # add in test for if health or benefits or NCA and redirect accordingly
     @services = Service.all.pluck(:service_level_1).uniq
     render json: @services
   end
 
   def service_level_2
-    #add in test for if health or benefits or NCA and redirect accordingly
-    @services = Service.where(service_level_1: params[:service_level_1]).pluck(:service_level_2).uniq
+    # add in test for if health or benefits or NCA and redirect accordingly
+    @services = Service.where(
+      service_level_1: params[:service_level_1]).pluck(:service_level_2).uniq
     render json: @services
+  end
+
+  def generate_hash_of_service_levels(services)
+    services_rolled_up = roll_up_services(services)
+    services_hash = {}
+    services_rolled_up.each do |service_level_1|
+      services_hash[service_level_1] = add_to_services_hash(
+        services, services_hash, service_level_1)
+    end
+    services_hash
+  end
+
+  def add_to_services_hash(services, _services_hash, service_level_1)
+    temp_array = []
+    services.each do |service|
+      if service_level_1 == service.service_level_1
+        temp_array << service.service_level_2 unless service.service_level_2.nil?
+      end
+    end
+    temp_array
+  end
+
+  def roll_up_services(services)
+    services_rolled_up = []
+    services.each do |service|
+      unless services_rolled_up.include?(service.service_level_1)
+        services_rolled_up << service.service_level_1
+      end
+    end
+    services_rolled_up
   end
 end
--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,13 +1,13 @@
 FactoryGirl.define do
   factory :facility do |f|
-    f.ID "402"
-    f.Location_Descriptive_Name_Common_Name "Omaha VAMC"
-    f.Classification "VA Medical Center (VAMC)"
+    f.facility_id "402"
+    f.location_descriptive_name_common_name "Omaha VAMC"
+    f.classification "VA Medical Center (VAMC)"
   end
 
   factory :service do |f|
-    f.Facility_ID "402"
-    f.Service_Level_1 "Complementary & Alternative Medicine"
-    f.Service_Level_2 "Chiropractic  Services"
+    f.facility_id "402"
+    f.service_level_1 "Complementary & Alternative Medicine"
+    f.service_level_2 "Chiropractic  Services"
   end
 end
