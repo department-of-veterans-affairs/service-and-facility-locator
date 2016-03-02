4b5c5ccfad3a93e2a89ed16dc2333e51bdb8cde1
Alex Yale-Loehr <ayaleloehr@gmail.com>
2016-03-01 23:22:30 -0500

    First commit to rework UX

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,12 +7,58 @@ class SearchController < ApplicationController
   end
 
   def show
-    @facility = Facility.where(Classification: params[:Classification])
-    render json: @facility.to_json
+    respond_to do |format|
+      format.html
+      format.json do
+        if params[:service_level_2].nil? and params[:service_level_1].nil?
+          @facilities = Facility.all
+        else
+          if !params[:service_level_2].nil?
+            facility_ids_that_match = Service.where(service_level_2: params[:service_level_2]).uniq.pluck(:facility_id)
+          else
+            facility_ids_that_match = Service.where(service_level_1:
+                          params[:service_level_1]).uniq.pluck(:facility_id)
+          end
+          @facilities = Facility.where(facility_id: facility_ids_that_match)
+        end
+        render json: @facilities
+      end
+    end
   end
 
   def single_facility
-    @facility = Facility.where(ID: params[:ID]).first
-    @services = Service.where(Facility_ID: params[:ID])
+    @facility = Facility.where(facility_id: params[:facility_id]).first
+    services = Service.where(facility_id: params[:facility_id])
+    @servicesRolledUp = []
+    services.each do | service |
+      unless @servicesRolledUp.include?(service.service_level_1)
+        @servicesRolledUp << service.service_level_1
+      end
+    end
+    @servicesHash = {}
+    @servicesRolledUp.each do | serviceLevel1 |
+      tempArray = []
+      services.each do | service |
+        if serviceLevel1 == service.service_level_1
+          if service.service_level_2 != nil
+            tempArray << service.service_level_2
+          end
+        end
+        @servicesHash[serviceLevel1] = tempArray
+      end
+    end
+    i = 2
+  end
+  
+  def service_types_by_administration
+    #add in test for if health or benefits or NCA and redirect accordingly
+    @services = Service.all.pluck(:service_level_1).uniq
+    render json: @services
+  end
+
+  def service_level_2
+    #add in test for if health or benefits or NCA and redirect accordingly
+    @services = Service.where(service_level_1: params[:service_level_1]).pluck(:service_level_2).uniq
+    render json: @services
   end
 end
--- db/migrate/20160209170151_create_facilities.rb
+++ db/migrate/20160209170151_create_facilities.rb
@@ -1,8 +1,31 @@
 class CreateFacilities < ActiveRecord::Migration
   def change
     create_table :facilities do |t|
-      t.string :ID
-      t.string :Official_Station_Name
+      t.string :facility_id
+      t.string :official_station_name
+      t.string :location_descriptive_name_common_name
+      t.string :classification
+      t.string :street_address
+      t.string :suite_building_number
+      t.string :city
+      t.string :state
+      t.string :zip_code
+      t.string :station_phone_number
+      t.string :station_fax_number
+      t.string :after_hours_phone_number
+      t.string :pharmacy_phone_number
+      t.string :enrollment_coordinator_phone_number
+      t.string :patient_advocate_phone_number
+      t.decimal :latitude
+      t.decimal :longitude
+      t.string :monday_hours
+      t.string :tuesday_hours
+      t.string :wednesday_hours
+      t.string :thursday_hours
+      t.string :friday_hours
+      t.string :saturday_hours
+      t.string :sunday_hours
+      t.text :hours_note
 
       t.timestamps null: false
     end
