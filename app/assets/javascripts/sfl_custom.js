5a6ceb008c9ad88e6757d1e25828c9b3f420388c
rickleegit <ricklee.va@gmail.com>
2016-03-14 17:41:26 -0400

    Added tab pattern to top of services column

--- app/assets/javascripts/sfl_custom.js
+++ app/assets/javascripts/sfl_custom.js
@@ -1,1 +1,15 @@
 $(document).ready(function(){$('#index-modal').foundation('reveal', 'open');});
+
+jQuery(document).ready(function() {
+    jQuery('.tabs .tab-links a').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+ 
+        // Show/Hide Tabs
+        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+ 
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+ 
+        e.preventDefault();
+    });
+});
